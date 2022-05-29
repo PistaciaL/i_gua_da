da35b783d8243b7bd984899246d8bc24044c9806
PistaciaL <523656497@qq.com>
2022-05-29 11:02:00 +0800

    first commit

---
+++ back/i_gua_da/src/main/java/org/nwpu/i_gua_da/IGuaDaApplication.java
@@ -0,0 +1,13 @@
+package org.nwpu.i_gua_da;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IGuaDaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IGuaDaApplication.class, args);
+    }
+
+}
---
+++ back/i_gua_da/src/test/java/org/nwpu/i_gua_da/IGuaDaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nwpu.i_gua_da;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IGuaDaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
