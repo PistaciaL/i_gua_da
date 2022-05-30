68628cba147c7cd6e34e753086a468c287e2a8e3
PistaciaL <523656497@qq.com>
2022-05-30 18:16:22 +0800

    email sender

---
+++ src/main/java/org/nwpu/i_gua_da/util/EmailSender.java
@@ -0,0 +1,67 @@
+package org.nwpu.i_gua_da.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+@Component
+public class EmailSender {
+    /* 邮箱账户名 */
+    @Value("${email.name}")
+    private String from;
+    /* 邮箱登录密码 */
+    @Value("${email.password}")
+    private String password;
+    /* 邮件服务器地址(这里是网易邮箱服务器) */
+    @Value("${email.host}")
+    private String host;
+    private Properties properties;
+    private Session session;
+
+    @PostConstruct
+    private void initField(){
+        this.properties = System.getProperties();
+        this.properties.put("mail.smtp.host", host);
+        this.properties.put("mail.smtp.auth", "true");
+        Authenticator auth = new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, password);
+            }
+        };
+        this.session = Session.getDefaultInstance(properties,auth);
+    }
+
+    /**
+     * 发送邮件
+     * @param to 收件人邮箱地址
+     * @param title 邮件标题
+     * @param message 邮件内容
+     */
+    public void send(String to, String title, String message) {
+        try {
+            MimeMessage mimeMessage = new MimeMessage(session);
+            mimeMessage.setFrom(new InternetAddress(this.from));
+            mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            mimeMessage.setSubject(title);
+            mimeMessage.setText(message);
+            Transport.send(mimeMessage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+//
+//    /**
+//     * 测试
+//     * @param args
+//     */
+//    public static void main(String[] args) {
+//        EmailSender sender = new EmailSender();
+//        sender.send("1026792451@qq.com", "验证码", "123456789");
+//    }
+}
--- src/test/java/org/nwpu/i_gua_da/IGuaDaApplicationTests.java
+++ src/test/java/org/nwpu/i_gua_da/IGuaDaApplicationTests.java
@@ -2,18 +2,22 @@ package org.nwpu.i_gua_da;
 
 import org.junit.jupiter.api.Test;
 import org.nwpu.i_gua_da.mapper.TestMapper;
+import org.nwpu.i_gua_da.util.EmailSender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class IGuaDaApplicationTests {
 
+//    @Autowired
+//    private TestMapper testMapper;
     @Autowired
-    private TestMapper testMapper;
+    private EmailSender emailSender;
 
     @Test
     void contextLoads() {
-        System.out.println(testMapper.testSelect());
+//        System.out.println(testMapper.testSelect());
+        emailSender.send("523656497@qq.com", "验证码", "123456");
     }
 
 }
